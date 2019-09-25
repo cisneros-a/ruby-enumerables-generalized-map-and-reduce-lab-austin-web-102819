f95967ce58546c7555f12fb938587621700cb6dc
Adrian Cisneros <cisneros_adrian51@yahoo.com>
2019-09-25 21:40:58 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,30 @@
-# Your Code Here
+def make_sandwich(element1, element2)
+  base = "A #{element1} and #{element2} sandwich"
+  puts base
+  yield
+  base
+end
+
+make_sandwich("chicken", "a sense of malaise") do |innards|
+  puts "making some tasty stuff..."
+end
+
+def make_sandwich_2(element1, element2)
+  base = "A #{element1} and #{element2}"
+  base2 = "Only a #{element1}"
+  puts yield(base2)
+end
+
+make_sandwich_2("gator", "gumbo") do |innards|
+  "#{innards} on rye"
+  puts "aaa"
+end
+
+
+array = [2, -4, 8, 10]
+
+def my_own_map(array)
+  yield(array.join(" "))
+end
+
+my_own_map(array) {|a| p a }
